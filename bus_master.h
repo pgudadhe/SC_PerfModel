741e83fc1571744aee3917ecd69d34da47f28f97
Pradnyesh Gudadhe <pgudadhe@users.noreply.github.com>
2025-07-18 17:31:42 -0700

    Update bus_master.h
    Added thread code

--- bus_master.h
+++ bus_master.h
@@ -7,6 +7,7 @@ class bus_master: sc_module
 {
 public:
   tlm_utils::simple_initiator_socket<bus_master> socket;
+  tlm_utils::peq_with_cb_and_phase<AXIMaster> peq;
 
   SC_HAS_PROCESS(bus_master);
 
@@ -14,6 +15,7 @@ public:
   : sc_module(name), 
     m_num_transactions(num_transactions),
     m_r_to_wr_ratio(ratio),
+    m_num_trans_in_progress(0),
     socket("master_socket")
   {
     SC_THREAD(thread_process);
@@ -25,6 +27,7 @@ private:
   unsigned int    m_data[16];
   unsigned int    m_num_transactions;
   float           m_r_to_wr_ratio;
+  unsigned int    m_num_trans_in_progress;
 
 };
 
