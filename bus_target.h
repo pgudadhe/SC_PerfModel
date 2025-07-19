2ab558c42a7100fcd4c07edad2b00e91af7d816d
Pradnyesh Gudadhe <pgudadhe@users.noreply.github.com>
2025-07-18 23:27:02 -0700

    Update bus_target.h
    Added FW NB transport

--- bus_target.h
+++ bus_target.h
@@ -8,9 +8,13 @@ class bus_target: sc_module
 public:
   tlm_utils::simple_target_socket<bus_target> socket;
 
-  SC_CTOR(bus_target) : socket("target_socket")
+  bus_target(sc_core::sc_module_name name) 
+  : sc_module(name), 
+    socket("target_socket")
   {
+    socket.register_nb_transport_fw(this, &AXISlave::nb_transport_fw);
   }
+
 
 };
 
